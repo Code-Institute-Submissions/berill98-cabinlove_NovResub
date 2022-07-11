25f63d923e65b87c9921247b0e24b1d6b49559d8
berill98 <berill0407@gmail.com>
2022-07-11 21:13:43 +0000

    Create Add Cabin page

--- cabinlove/routes.py
+++ cabinlove/routes.py
@@ -22,3 +22,25 @@ def add_location():
         db.session.commit()
         return redirect(url_for("locations"))
     return render_template("add_location.html")
+
+
+@app.route("/add_cabin", methods=["GET", "POST"])
+def add_cabin():
+    locations = list(Location.query.order_by(Location.location_name).all())
+    if request.method == "POST":
+        cabin = Cabin(
+            cabin_name = request.form.get("cabin_name"),
+            photo = request.form.get("photo_url"),
+            cabin_description = request.form.get("cabin_description"),
+            pet_friendly = bool(True if request.form.get("pet_friendly") else False),
+            wifi_included = bool(True if request.form.get("wifi_included") else False),
+            kids_allowed = bool(True if request.form.get("kids_allowed") else False),
+            max_adults = request.form.get("max_adults"),
+            price_per_night = request.form.get("price_per_night"),
+            location_id = request.form.get("location_id"),
+            created_by = request.form.get("created_by"),
+        )
+        db.session.add(cabin)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_cabin.html", locations=locations)
